62264dafadef1855fc6050bcd00cc8a63397bebd
Armanix <a.shaikh@student.saedubai.com>
2023-06-16 15:54:14 +0400

    Dialogue system, tileset Bug fix, player colliders

---
+++ Assets/NPC.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class NPC : MonoBehaviour
+{
+    public GameObject dialoguePanel;
+    public TextMeshProUGUI dialogueText;
+    public TextMeshProUGUI Name;
+    public string npcName;
+    public string[] dialogue;
+    private int index;
+
+    public float wordSpeed;
+    public bool playerIsClose;
+
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && playerIsClose)
+        {
+            if(dialoguePanel.activeInHierarchy)
+            {
+                zerotext();
+            }
+            else
+            {
+                dialoguePanel.SetActive(true);
+                StartCoroutine(Typing());
+            }
+        }
+
+        Name.text = npcName;
+    }
+
+    public void zerotext()
+    {
+        dialogueText.text = "";
+        index = 0;
+        dialoguePanel.SetActive(false);
+    }
+
+    IEnumerator Typing()
+    {
+        foreach(char letter in dialogue[index].ToCharArray())
+        {
+            dialogueText.text+= letter;
+            yield return new WaitForSeconds(wordSpeed);
+        }
+    }
+
+    public void NextLine()
+    {
+        if(index < dialogue.Length - 1)
+        {
+            index++;
+            dialogueText.text = "";
+            StartCoroutine(Typing());
+        }
+        else
+        {
+            zerotext();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            playerIsClose = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerIsClose = false;
+            zerotext();
+        }
+    }
+
+}
