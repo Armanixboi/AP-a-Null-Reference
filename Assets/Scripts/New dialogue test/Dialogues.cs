d226a5db693e675c6ada273dfccda840eba6980b
Armanix <a.shaikh@student.saedubai.com>
2023-06-21 15:15:13 +0400

    stuck on dialogue branching

--- Assets/Scripts/New dialogue test/Dialogues.cs
+++ Assets/Scripts/New dialogue test/Dialogues.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Dialogues : MonoBehaviour
 {
     [SerializeField]
-    private string[] sentences;
+    private string[] sentences1;
+    [SerializeField]
+    private string[] sentences2;
+
     public GameObject[] answers;
+    public TextMeshProUGUI dialogueText;
     void Start()
     {
         for (int i = 0; i < answers.Length; i++)
@@ -19,5 +25,23 @@ public class Dialogues : MonoBehaviour
     void Update()
     {
         
+    }
+    public void DialogueOption1()
+    {
+        dialogueText.gameObject.SetActive(true);
+        for (int i = 0; i < answers.Length; i++)
+        {
+            answers[i].SetActive(false);
+        }
+        dialogueText.text = (sentences1[0]);
+    }
+    public void DialogueOption2()
+    {
+        dialogueText.gameObject.SetActive(true);
+        for (int i = 0; i < answers.Length; i++)
+        {
+            answers[i].SetActive(false);
+        }
+        dialogueText.text = (sentences2[0]);
     }
 }
