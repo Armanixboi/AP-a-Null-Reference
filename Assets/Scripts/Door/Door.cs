2144d54bd1e3b88f24a7a59fe6a25803fd294e68
Armanix <a.shaikh@student.saedubai.com>
2023-06-23 16:04:13 +0400

    Door Animations

---
+++ Assets/Scripts/Door/Door.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public Animator door;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        door.SetBool("dooropen", true);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        door.SetBool("dooropen", false);
+    }
+}
