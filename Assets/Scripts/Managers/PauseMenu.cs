eb27a06e8c84426f9997a0aa0e0e0e9464934c57
Armanix <a.shaikh@student.saedubai.com>
2023-07-12 02:32:47 +0400

    Pause Menu

---
+++ Assets/Scripts/Managers/PauseMenu.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenuPanel;
+
+    void Update()
+    {
+        
+    }
+
+    public void Pause()
+    {
+        pauseMenuPanel.SetActive(true);
+        Time.timeScale = 0;
+    }
+    public void Continue()
+    {
+        pauseMenuPanel.SetActive(false);
+        Time.timeScale = 1;
+    }
+    public void MainMenuScene()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+    public void Restart()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+}
