c10c899a5471cc421f9d4a30ddd47c5733134a55
Armanix <a.shaikh@student.saedubai.com>
2023-07-17 00:09:19 +0400

    Last Level and bug fixing

---
+++ Assets/Scripts/EndGame.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGame : MonoBehaviour
+{
+    public bool canEndIt;
+    public Animator startWorkingUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canEndIt == true)
+        {
+            startWorkingUI.SetBool("canpickup", true);
+        }
+        if (canEndIt == false)
+        {
+            startWorkingUI.SetBool("canpickup", false);
+        }
+    }
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            canEndIt = true;
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            canEndIt = false;
+        }
+    }
+}
